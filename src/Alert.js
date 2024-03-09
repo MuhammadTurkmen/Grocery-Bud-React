f6e3ea540f969b6f78e17fec3cd7efcbb3bc1ee4
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-09 11:18:51 +0430

    init

--- src/Alert.js
+++ src/Alert.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react'
 const Alert = ({type, msg, removeAlert}) => {
   useEffect(() => {
     const timeout = setTimeout(() => {
-
+      removeAlert()
     }, 3000)
   },[])
   return <p className={`alert alert-${type}`}>{msg}</p>
