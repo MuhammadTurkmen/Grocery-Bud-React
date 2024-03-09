ae56e9de19cbee889f700bf8d9b36b1c2b8c2ee9
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-09 10:47:34 +0430

    init

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
   const [alert, setAlert] = useState({show: false, msg: '', type:''})
   const handleSubmit = (e) => {
     e.preventDefault()
+    if(!name) {
+      
+    }
   } 
   return <section className='section-center'>
     <form className='grocery-form' onSubmit={handleSubmit}>
