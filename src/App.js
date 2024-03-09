58e6540ee7841ec885a21ce10eaca6bc42cbbfeb
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-09 11:14:21 +0430

    init

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     e.preventDefault()
     if(!name) {
       // display alert
-      setAlert({show: true, msg: 'please enter value', type: 'danger'})
+      showAlert(true, 'danger', 'please enter value')
     }
     else if(name && isEditing) {
       // deal with edit
