aecadca7b009d8f7e68c6a98cc461070f3759f35
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-09 10:57:10 +0430

    init

--- src/List.js
+++ src/List.js
@@ -5,7 +5,9 @@ const List = ({items}) => {
   return <div className="grocery-list">
     {items.map((item) => {
       const {id, title} = item
-      return <article key={id} className='grocery-item'></article>
+      return <article key={id} className='grocery-item'>
+        <p className='title'>{title}</p>
+      </article>
     })}
   </div>
 }
