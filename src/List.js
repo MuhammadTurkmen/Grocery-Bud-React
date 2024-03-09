e461e5f31dcc3f385dfd2d543962cfa5facd30d8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-09 10:55:56 +0430

    init

--- src/List.js
+++ src/List.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
 const List = ({items}) => {
   
-  return <div className="grocery-list"></div>
+  return <div className="grocery-list">
+    {items.map((item) => {
+      
+    })}
+  </div>
 }
 
 export default List
