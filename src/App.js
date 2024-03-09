8be0d3d077a3cc3bffe3a1dfab8d8ab4c4c8f54d
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-09 10:43:14 +0430

    init

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       {alert.show && <Alert />}
       <h3>grocery bud</h3>
       <div className="form-control">
-        <input type="text" name="" id="" />
+        <input type="text" />
         <button type='submit' className='submit-btn'>
           {isEditing ? 'edit' : 'submit'}
         </button>
