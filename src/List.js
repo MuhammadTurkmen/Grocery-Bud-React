d4d7599eba7e320d570d39f846dd782111801055
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-10 11:30:33 +0430

    init

--- src/List.js
+++ src/List.js
@@ -11,7 +11,7 @@ const List = ({items, removeItem}) => {
           <button type='button' className='edit-btn'>
             <FaEdit />
           </button>
-          <button type='button' className='delete-btn'>
+          <button type='button' className='delete-btn' onClick={() => removeItem(id)}>
             <FaTrash />
           </button>
         </div>
