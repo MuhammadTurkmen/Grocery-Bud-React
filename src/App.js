6f6413f403ce0ea56c81078cfa0aca73b230ab39
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-10 11:59:42 +0430

    init

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const getLocalStorage = () => {
 
 function App() {
   const [name, setName] = useState('')
-  const [list, setList] = useState([])
+  const [list, setList] = useState(getLocalStorage())
   const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditID] = useState(null)
   const [alert, setAlert] = useState({
